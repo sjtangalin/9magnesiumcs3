d8a1fae89e4e06c794faf1dc2da0580759203c5e
sjtangalin <sjotangalin@brc.pshs.edu.ph>
2026-08-19 17:39:06 +0800

    Add Chinese Zodiac program based on birth year

---
+++ q1/zodiacMagnesiumTangalin.py
@@ -0,0 +1,26 @@
+# Chinese Zodiac Program
+
+birth_year = int(input("Enter your birth year: "))
+
+if birth_year < 1900:
+    print("Invalid Year, it should not be earlier than 1900")
+else:
+    zodiac_signs = [
+        "Rat (鼠 / Shǔ)",
+        "Ox (牛 / Niú)",
+        "Tiger (虎 / Hǔ)",
+        "Rabbit (兔 / Tù)",
+        "Dragon (龙 / Lóng)",
+        "Snake (蛇 / Shé)",
+        "Horse (马 / Mǎ)",
+        "Goat (羊 / Yáng)",
+        "Monkey (猴 / Hóu)",
+        "Rooster (鸡 / Jī)",
+        "Dog (狗 / Gǒu)",
+        "Pig (猪 / Zhū)"
+    ]
+
+    zodiac_index = (birth_year - 1900) % 12
+    zodiac = zodiac_signs[zodiac_index]
+
+    print("Your Chinese Zodiac Sign is:", zodiac)
